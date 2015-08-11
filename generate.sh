1a27f29e0fb16db6b713d2a1895130c879b45b39
coldclimate <oliver@coldclimate.co.uk>
2015-08-11 20:13:50 +0100

    adjusting paths so this wil work

--- generate.sh
+++ generate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -eux
-for i in `ls images/fullsize/`
+for i in `ls output/images/fullsize/`
 do
 echo ${i}
-convert -thumbnail 200 images/fullsize/${i} images/thumbs/${i}
+convert -thumbnail 200 output/images/fullsize/${i} output/images/thumbs/${i}
 echo "<a class=\"example-image-link\" href=\"/images/fullsize/${i}\" data-lightbox=\"example-set\"><img class=\"example-image\" src=\"/images/thumbs/${i}\" alt=\\""/></a>" >> list.txt
 done
-cp boxed/top.html index.html
-cat list.txt >> index.html
-cat boxed/bottom.html >> index.html
+cp boxed/top.html output/index.html
+cat list.txt >> output/index.html
+cat boxed/bottom.html >> output/index.html
 source boxed/variables.txt
-sed -i 's/@@TITLE@@/'"${TITLE}"'/g' index.html
-sed -i 's/@@INTRO@@/'"${INTRO}"'/g' index.html
-rm generate.sh
+sed -i 's/@@TITLE@@/'"${TITLE}"'/g' output/index.html
+sed -i 's/@@INTRO@@/'"${INTRO}"'/g' output/index.html
+rm list.txt
